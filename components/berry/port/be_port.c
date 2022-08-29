a029219d204154576a255ab1b6f433e52a784bec
egonzalez <egonzalez@msi.notebook.hiperion.local>
2022-08-29 00:01:39 -0300

    Minimal corrections

--- components/berry/port/be_port.c
+++ components/berry/port/be_port.c
@@ -1,10 +1,30 @@
-/********************************************************************
- ** Copyright (c) 2018-2020 Guan Wenliang
- ** This file is part of the Berry default interpreter.
- ** skiars@qq.com, https://github.com/Skiars/berry
- ** See Copyright Notice in the LICENSE file or at
- ** https://github.com/Skiars/berry/blob/master/LICENSE
- ********************************************************************/
+/*
+ * Copyright 2022 Emiliano Gonzalez (egonzalez . hiperion @ gmail . com))
+ * * Project Site: https://github.com/hiperiondev/esp32-berry-lang *
+ *
+ * This is based on other projects:
+ *    Berry (https://github.com/berry-lang/berry)
+ *    LittleFS port for ESP-IDF (https://github.com/joltwallet/esp_littlefs)
+ *    Lightweight TFTP server library (https://github.com/lexus2k/libtftp)
+ *    Others (see individual files)
+ *
+ *    please contact their authors for more information.
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
 
 #include <stdio.h>
 #include <string.h>
@@ -14,7 +34,6 @@
 #include "be_sys.h"
 
 #include "internals.h"
-
 
 /* this file contains configuration for the file system. */
 
--- main/main.c
+++ main/main.c
@@ -41,11 +41,12 @@
 #include "linenoise/linenoise.h"
 
 #include "esp_littlefs.h"
+#include "littlefs.h"
 #include "berry.h"
 #include "be_mapping.h"
 #include "be_portutils.h"
 
-static char *TAG = "main";
+//static char *TAG = "main";
 #define CONSOLE_UART_CHANNEL  UART_NUM_0
 
 static bbool is_multline(bvm *vm) {
@@ -129,37 +130,11 @@ int repl_line(bvm *vm, char *line) {
 }
 
 void repl_task(void *arg) {
-    ESP_LOGI(TAG, "Initializing LittleFS");
+    int probe_status;
+    char *line;
+    bvm *vm = NULL;
 
-    esp_vfs_littlefs_conf_t conf = {
-            .base_path = "/littlefs",
-            .partition_label = "littlefs",
-            .format_if_mount_failed = true,
-            .dont_mount = false,
-    };
-
-    // Use settings defined above to initialize and mount LittleFS filesystem.
-    // Note: esp_vfs_littlefs_register is an all-in-one convenience function.
-    esp_err_t ret = esp_vfs_littlefs_register(&conf);
-
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-            ESP_LOGE(TAG, "Failed to mount or format filesystem");
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            ESP_LOGE(TAG, "Failed to find LittleFS partition");
-        } else {
-            ESP_LOGE(TAG, "Failed to initialize LittleFS (%s)", esp_err_to_name(ret));
-        }
-        return;
-    }
-
-    size_t total = 0, used = 0;
-    ret = esp_littlefs_info(conf.partition_label, &total, &used);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to get LittleFS partition information (%s)", esp_err_to_name(ret));
-    } else {
-        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
-    }
+    littlefs_init();
 
     // Initialize VFS & UART so we can use std::cout/cin
     setvbuf(stdin, NULL, _IONBF, 0);
@@ -174,7 +149,7 @@ void repl_task(void *arg) {
     /* Move the caret to the beginning of the next line on '\n' */
     esp_vfs_dev_uart_port_set_tx_line_endings(CONSOLE_UART_CHANNEL, ESP_LINE_ENDINGS_CRLF);
 
-    int probe_status = linenoiseProbe();
+    probe_status = linenoiseProbe();
     if (probe_status) {
         /* zero indicates success */
         linenoiseSetDumbMode(1);
@@ -187,12 +162,9 @@ void repl_task(void *arg) {
         //printf("\033[2J\033[1H");
         printf("\n");
     }
+    line = NULL;
 
-    char *line = NULL;
-
-    bvm *vm = NULL;
     bool status = BerryInit(&vm, NULL);
-
     printf("< BerryInit: %s >\n\n", status ? "ok" : "fail!");
 
     printf("-- Minimal test --\n");
